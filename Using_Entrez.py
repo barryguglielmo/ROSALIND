af8fd0fa45cb4c2930639751ccf0d5523f38c599
barryguglielmo <barryguglielmo@gmail.com>
2017-12-23 15:37:17 -0500

    Update and rename Prob 12.py to Using_Entrez.py

--- Using_Entrez.py
+++ Using_Entrez.py
@@ -1,7 +1,6 @@
 from Bio import Entrez
-# I found this example code online *https://github.com/adelq/rosalind/commit/5472a340628dc13774a7a0b833724466914136b4
-# Don't reinvent the wheel if you dont have to
-# Define the function
+#http://biopython.org/DIST/docs/api/Bio.Entrez-module.html
+d# Define the function
 def genbank(genus, dtstart, dtend):
 	Entrez.email = "barryguglielmo@gmai.com" 
 	handle = Entrez.esearch(db="nucleotide", term='%s[Organism] AND (%s[Publication Date] : %s[Publication Date])' % (genus, dtstart, dtend))
